14993bf9c63b54b2ef5c64cf9e2fa92f437d8fbf
mkrzyzyn <maciej.krzyzynski@gmail.com>
2021-10-23 18:27:53 +0200

    Stage 4/6.

--- Simple Search Engine/task/src/search/Main.java
+++ Simple Search Engine/task/src/search/Main.java
@@ -3,9 +3,7 @@ package search;
 public class Main {
     public static void main(String[] args) {
 
-        SearchEngine se = new SearchEngine();
+        SearchEngine se = new SearchEngine(args);
         se.runMenu();
-
-        //System.out.println("Hello World!");
     }
 }
--- Simple Search Engine/task/src/search/SearchEngine.java
+++ Simple Search Engine/task/src/search/SearchEngine.java
@@ -2,6 +2,8 @@ package search;
 
 import org.w3c.dom.ls.LSOutput;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -15,16 +17,24 @@ public class SearchEngine {
     private String inputNumberQueries;
     private String choices;
 
+    public SearchEngine(String[] args) {
+        this.args = args;
+    }
 
-    private List<String> listOfPeople = new ArrayList<String>();
+    private final String[] args;
+
+
+    private final List<String> listOfPeople = new ArrayList<>();
 
     Scanner sc = new Scanner(System.in);
 
-    public void runMenu(){
+    public void runMenu() {
 
-        collectData();
+        // collectData();
 
-        while(true) {
+        readFile(args);
+
+        while (true) {
 
             System.out.println("=== Menu ===\n" +
                     "1. Find a person\n" +
@@ -50,26 +60,25 @@ public class SearchEngine {
         }
 
 
-
     }
 
-    public void find(){
+    public void find() {
 
-            System.out.println("Enter a name or email to search all suitable people.");
-            inputSearchElement = sc.nextLine();
-            outputResults(searchStringInList(listOfPeople, inputSearchElement));
+        System.out.println("Enter a name or email to search all suitable people.");
+        inputSearchElement = sc.nextLine();
+        outputResults(searchStringInList(listOfPeople, inputSearchElement));
 
     }
 
     public List<String> searchStringInList(List<String> list, String searchElement) {
-    
-    return list.stream().filter(el -> el.toLowerCase().contains(searchElement.toLowerCase())).collect(Collectors.toList());
-        
-    }
-    
-    public void outputResults(List<String> results){
 
-        if (results.size() == 0){
+        return list.stream().filter(el -> el.toLowerCase().contains(searchElement.toLowerCase())).collect(Collectors.toList());
+
+    }
+
+    public void outputResults(List<String> results) {
+
+        if (results.size() == 0) {
             System.out.println("No matching people found.");
         } else {
             System.out.println("Found people:");
@@ -78,25 +87,45 @@ public class SearchEngine {
 
     }
 
-    public void printAll(){
+    public void printAll() {
 
         System.out.println("=== List of people ===");
         listOfPeople.forEach(el -> System.out.println(el));
 
     }
 
-    public void collectData(){
+    public void collectData() {
 
         System.out.println("Enter the number of people:");
         inputNumberPeople = sc.nextLine();
 
         System.out.println("Enter all people:");
 
-        for(int i=0; i < Integer.parseInt(inputNumberPeople); i++){
+        for (int i = 0; i < Integer.parseInt(inputNumberPeople); i++) {
 
             listOfPeople.add(sc.nextLine());
         }
+    }
 
+    public void readFile(String[] parameters) {
+
+        String fileName="";
+
+        if (parameters[0].equals("--data")){
+            fileName = parameters[1];
+        }
+
+        try {
+            File myObj = new File(fileName);
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                listOfPeople.add(myReader.nextLine());
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
 
     }
 
